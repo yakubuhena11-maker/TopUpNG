61abb15eb15cf857d0586feb4c538fd47385c4c6
yakubuhena11-maker <yakubuhena11@gmail.com>
2026-08-24 07:08:48 +0100

    Update page.js

--- app/settings/page.js
+++ app/settings/page.js
@@ -2,11 +2,6 @@ import { redirect } from "next/navigation";
 import { getCurrentUser } from "@/lib/auth";
 import SettingsForm from "./SettingsForm";
 
-function initials(name, phone) {
-  if (name) return name.split(" ").map((n) => n[0]).slice(0, 2).join("").toUpperCase();
-  return phone.slice(-2);
-}
-
 export default async function SettingsPage() {
   const user = await getCurrentUser();
   if (!user) redirect("/login");
@@ -17,16 +12,9 @@ export default async function SettingsPage() {
         <div className="wordmark">
           top<span>up</span>ng
         </div>
-        <div className="profile-row" style={{ marginTop: 20 }}>
-          <div className="avatar">{initials(user.name, user.phone)}</div>
-          <div>
-            <div className="name">{user.name || "Add your name"}</div>
-            <div className="phone">{user.phone}</div>
-          </div>
-        </div>
 
         <SettingsForm user={user} />
       </div>
     </div>
   );
-  }
+}
